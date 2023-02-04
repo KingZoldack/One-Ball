63c7a66d3e00b1293fb51b424a2efecb6cce84a6
KingZoldack <jeanpaulismael@gmail.com>
2023-02-04 18:25:54 +0000

    Powerup
    Create powerup system. Create powerup, destroy power up on collection, implement powerup mechanics, powerup countdown and create UI for powerup timer.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,7 +12,15 @@ public class PlayerController : MonoBehaviour
     GameObject _focusPoint;
 
     [SerializeField]
+    Image _timerFill;
+
+    [SerializeField]
     float _speed = 5f;
+
+    [SerializeField]
+    float _powerupTimer = 7f;
+    float _timeLeft;
+    bool _hasPowerup = false;
 
     private void Awake()
     {
@@ -29,10 +38,41 @@ public class PlayerController : MonoBehaviour
         _playerInputActions.Player.Movement.Disable();
     }
 
+    private void Update()
+    {
+        if (_timeLeft > 0 && _hasPowerup)
+        {
+            _timeLeft -= Time.deltaTime;
+            _timerFill.fillAmount = _timeLeft / _powerupTimer;
+
+            if (_timeLeft <= 0)
+                _hasPowerup = false;
+        }
+    }
+
     private void FixedUpdate()
     {
         Vector2 actions = _playerInputActions.Player.Movement.ReadValue<Vector2>();
         _rb.AddForce(_focusPoint.transform.forward * actions.y *_speed);
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Powerup>() != null)
+        {
+            _hasPowerup = true;
+            Destroy(other.gameObject);
+            _timeLeft = 7f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Enemy>() && _hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 repelDirection = collision.gameObject.transform.position - transform.position;
+            enemyRb.AddForce(repelDirection * 10, ForceMode.Impulse);
+        }
     }
 }
